aa848b42af78cbd0559f20527d69e45c16738566
RedVinck <34350465+RedVinck@users.noreply.github.com>
2021-04-01 10:21:44 +0200

    Added for demo

--- src/main/java/com/redvinck/SpringBootProjectWeekCloud/ProductController.java
+++ src/main/java/com/redvinck/SpringBootProjectWeekCloud/ProductController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://cactus-2-shopping-cartv2-ucllteam02.ocp-ucll-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloud/", allowCredentials = "true")
+@CrossOrigin(origins = "http://cactus-2-shopping-cartv2-ucllteam02.ocp-ucll-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloudd", allowCredentials = "true")
 @RestController
 public class ProductController {
 
